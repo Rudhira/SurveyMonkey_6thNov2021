ff425ddec319553654be9aea84a5209dbce2fc0f
Rudhira <rudhira.katragadda@gmail.com>
2021-11-09 21:32:41 -0500

    sauce lab code

--- src/main/java/Basetest.java
+++ src/main/java/Basetest.java
@@ -3,12 +3,15 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
 import javax.imageio.IIOException;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.net.URL;
 import java.util.Properties;
 
 public class Basetest {
@@ -27,8 +30,22 @@ public class Basetest {
        // String browName = "Chrome";
         if (browsername.equalsIgnoreCase("Chrome")) {
 
-            WebDriverManager.chromedriver().setup();
-            driver = new ChromeDriver();
+            /*WebDriverManager.chromedriver().setup();
+            driver = new ChromeDriver();*/
+
+            DesiredCapabilities caps = DesiredCapabilities.chrome();
+            caps.setCapability("platform", "Windows 10");
+            caps.setCapability("version", "latest");
+            caps.setCapability("name", "Demo Test");
+            caps.setCapability("extendedDebugging", "true");
+            //caps.setCapability("tunnelIdentifier", "oauth-pavanimulkalapally-83b21_tunnel_id");
+
+            driver = new RemoteWebDriver(new URL("https://oauth-rudhira.katragadda-4673f:bbf3a26d-ac8a-4a69-a63f-577065766969@ondemand.us-west-1.saucelabs.com:443/wd/hub"), caps);
+            System.out.println("Thread ID Is : " + Thread.currentThread().getId());
+            String sessionID = ((RemoteWebDriver) driver).getSessionId().toString();
+            System.out.println("sessionID: "+sessionID);
+            //driver.get("https://www.surveymonkey.com/");
+
 
 
         } else if (browsername.equalsIgnoreCase("Edge")) {
